65ca390a2f89a1b6deaf68f783853cf1c2102cbc
joeyskeys <joey.skeys@gmail.com>
2022-10-26 11:23:12 +0800

    use pyzen to generate oso info

--- nodes/shading.py
+++ nodes/shading.py
@@ -1,4 +1,5 @@
-
+from genericpath import isfile
+import os
 import bpy
 from ..utils.registry import shading_node_registry, ShadingNode
 from ..ui.preferences import get_pref
@@ -36,8 +37,14 @@ def get_oso_info(oso_name, search_path):
     #ret = subprocess.run(oslinfo_cmd, shell=True, stdout=subprocess.PIPE)
     #return ret.stdout.decode('utf-8')
     q = osl.Querier(oso_name, search_path)
-    param_infos = []
+    shader_info = {
+        'name' : q.getshadername(),
+        'type' : q.getshadertype(),
+        'params' : []
+    }
+
     for i in range(q.nparams()):
+        '''
         baset = param.getparambasetype(i)
         dfts = None
         if baset in ('float', 'double'):
@@ -46,78 +53,31 @@ def get_oso_info(oso_name, search_path):
             dfts = q.getdefaultsi(i)
         else:
             dfts = q.getdefaultss(i)
-        param_infos.append((q.getparamname(i), q.getparamtype(i), dfts))
+        shader_info['params'].append((q.getparamname(i), q.getparamtype(i), dfts))
+        '''
+
+        param = q.getparam(i)
+        param_info = {
+            'name' : param.getname(),
+            'type' : param.gettype(),
+        }
+        metadatas = param.getmetadatas()
+        for metadata in metadatas:
+            metatype = metadata.getbasetype()
+            default = None
+            if metatype == 'int':
+                default = metadata.getdefaulti()
+            elif metatype == 'float':
+                default = metadata.getdefaultf()
+            else:
+                default = metadata.getdefaults()
+            param_info[metadata.getname()] = default
     
     return (q.shadername(), q.shadertype(), param_infos)
 
 
-def parse_param_info(param_line):
-    # Currently we don't have python binding to get the information of oso parameters,
-    # use the osl utility for now
-    comps = param_line.split(' ')
-    ptype = comps[0]
-    pname = comps[1]
-    pdefault = None
-    if ptype in ('color', 'point', 'vector', 'normal'):
-        pdefault = map(float, comps[3 : 6])
-    else:
-        # This works only for basic types
-        pdefault = eval('{}({})'.format(ptype, comps[2]))
-    return (ptype, pname, pdefault)
-
-
-def convert_param_value(ptype, pvalue_str):
-    comps = pvalue_str.strip('[ ]').split(' ')
-    val = None
-    if ptype in ('color', 'point', 'vector', 'normal'):
-        val = map(float, comps)
-    elif ptype == 'string':
-        val = pvalue_str
-    else:
-        val = eval('{}({})'.format(ptype, pvalue_str))
-
-    return val
-
-
-def parse_metadata(param_info):
-    in_params = False
-    param_name = None
-    shader_info = { 'params' : [] }
-    param_info = {}
-    param_type = None
-    for line in param_info:
-        line = line.strip()
-        if line.startswith('metadata:'):
-            comps = line[len('metadata: ') :].split(' ')
-            metatype = comps[0]
-            metaname = comps[1]
-            metavalue = comps[3]
-            if in_params:
-                param_info[metaname] = convert_param_value(metatype, metavalue)
-            else:
-                shader_info[metaname] = convert_param_value(metatype, metavalue)
-        elif line.startswith('Default value: '):
-            pvalue_str = line[len('Default value: ') :]
-            param_info['default'] = convert_param_value(param_type, pvalue_str)
-        elif line.startswith('surface') or line.startswith('volume') or line.startswith('displacement'):
-            comps = line.split(' ')
-            shader_info['type'] = comps[0]
-            shader_info['name'] = comps[1].strip('"')
-        else:
-            if len(param_info) > 0:
-                shader_info['params'].append(param_info)
-                param_info = {}
-
-            in_params = True
-            comps = line.split(' ')
-            param_info['name'] = comps[0].strip('"')
-            param_type = comps[1].strip('"')
-            param_info['type'] = param_type
-
-    return shader_info
-
-
 def load_osl_shaders():
+    '''
     perf = get_pref()
     osl_query_location = getattr(perf, 'osl_query_location', None)
     if not osl_query_location:
@@ -128,14 +88,18 @@ def load_osl_shaders():
     if not osl_shader_location:
         print('osl shader location not set')
         return {}
+    '''
 
     shader_infos = {}
-    if os.path.isdir(osl_shader_location):
-        for shader in os.listdir(osl_shader_location):
-            if shader.endswith('.oso'):
-                param_infos = []
-                info = get_oso_info(osl_query_location, os.path.join(osl_shader_location, shader))
-                shader_infos[shader.split('.')[0]] = parse_metadata(info)
+    shader_folder_path = './shader'
+    for shader_item in os.listdir(shader_folder_path):
+        full_path = os.path.join(shader_folder_path, shader_item)
+        if not os.isfile(full_path):
+            continue
+        if shader_item.endswith('.oso'):
+            filename = shader_item.split('.')[0]
+            oso_info = get_oso_info(filename, shader_folder_path)
+            shader_infos[filename] = oso_info
 
     return shader_infos
 
