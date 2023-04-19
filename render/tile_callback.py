7bc7d6c075afac9072af1b6c55523ab884f825f9
joeyskeys <joey.skeys@gmail.com>
2023-04-19 23:12:06 +0800

    add a print to debug tile callback function

--- render/tile_callback.py
+++ render/tile_callback.py
@@ -8,6 +8,12 @@ class TileCallback(pyzen.api.RenderCallback):
         self.rendered_tile_cnt = 0.0
         self.render_status = "Starting..."
 
+    @staticmethod
+    def convert_to_tuples(floats):
+        pixels = []
+        for i in range(0, len(floats), 3):
+            pixels.append((floats[i], floats[i + 1], floats[i + 2]))
+
     def on_tile_end(self, film, tile_id):
         print('tile end for id : ', tile_id)
         tile = film.tiles[tile_id]
@@ -16,6 +22,8 @@ class TileCallback(pyzen.api.RenderCallback):
             tile.width, tile.height)
         render_view = self.engine.active_view_get()
         layer = result.layers[0].passes.find_by_name("Combined", render_view)
+        #floats = tile.get_data()
+        #pixels = self.convert_to_tuples(floats)
         pixels = tile.get_data()
         layer.rect = pixels
 
