9f0974cdbef9d0be6e44d4faa8b27bb21072868a
joeyskeys <joey.skeys@gmail.com>
2023-06-04 23:06:07 +0800

    refactoring code

--- operators/debug.py
+++ operators/debug.py
@@ -9,17 +9,10 @@ class DebugRenderOperator(bpy.types.Operator):
     bl_label = 'Debug'
     bl_description = 'Invoke render process for a specific pixel'
 
-    mouse_x: bpy.props.IntProperty(name="Mouse X Coordinate")
-    mouse_y: bpy.props.IntProperty(name="Mouse Y Coordinate")
-
     @classmethod
     def poll(cls, context):
         return True
 
-    def execute(self, ctx):
-        self.report({'INFO'}, 'input {}, {}, move {}, {}'.format(self.mouse_x, self.mouse_y, self.x, self.y))
-        return {'FINISHED'}
-    
     @staticmethod
     def find_window_region(space):
         for region in space.regions:
@@ -27,14 +20,18 @@ class DebugRenderOperator(bpy.types.Operator):
                 return region
         raise Exception('Cannot find window region')
 
+    def execute(self, ctx):
+        return {'FINISHED'}
+
     def modal(self, ctx, event):
         if event.type == 'MOUSEMOVE':
-            self.x = event.mouse_x
-            self.y = event.mouse_y
+            pass
+        elif event.type == 'LEFTMOUSE':
             region = self.find_window_region(ctx.area)
             u, v = region.view2d.region_to_view(event.mouse_region_x, event.mouse_region_y)
             if u < 0 or u > 1 or v < 0 or v > 1:
-                pass
+                print('clicked pixel out of image, cancelled')
+                return {'CANCELLED'}
 
             # Very weird problem, the size for Render Result is 0, 0
             # Temporarily use render resolution, but notice this will introduce
@@ -42,26 +39,31 @@ class DebugRenderOperator(bpy.types.Operator):
             # Imagine the user changed render resolution and run the operator
             # before he/she actually re-render the image
             # Perhaps we should explicitly set the image size after render
-            # finished?
-            img_size = bpy.data.images['Render Result'].size
+            # finished?(Confirmed, read-only property)
+            #
+            # Update: https://blender.stackexchange.com/questions/2170/how-to-access-render-result-pixels-from-python-script
+            # This thread discusses the problem, answer by Iyad is viable but
+            # it doesn't work during my test
+            #
+            #img_size = bpy.data.images['Render Result'].size
             #img_width = img_size[0]
             #img_height = img_size[1]
+
             img_width = ctx.scene.render.resolution_x
             img_height = ctx.scene.render.resolution_y
-            self.pixel_x = int(u * img_width)
-            self.pixel_y = int(v * img_height)
-            print('x, y', event.mouse_region_x, event.mouse_region_y, self.pixel_x, self.pixel_y)
-        elif event.type == 'LEFTMOUSE':
-            print('finished', self.pixel_x, self.pixel_y)
+            pixel_x = int(u * img_width)
+            pixel_y = int(v * img_height)
+
+            # TODO: Invoke the debug render here...
+            print('finished', pixel_x, pixel_y)
             return {'FINISHED'}
+            
         elif event.type in {'RIGHTMOUSE', 'ESC'}:
             return {'CANCELLED'}
         
         return {'RUNNING_MODAL'}
     
     def invoke(self, ctx, event):
-        self.x = event.mouse_x
-        self.y = event.mouse_y
         wm = ctx.window_manager
         wm.modal_handler_add(self)
         return {'RUNNING_MODAL'}
